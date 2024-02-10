b613bf0bde19a2debf71967bcc20e9d7165dc9eb
Kumar Chaudhary <kumarbhetwal25@gmail.com>
2024-02-10 22:37:09 +0545

    Add PrismaModule to AuthModule and export PrismaService from PrismaModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { PrismaModule } from "src/prisma/prisma.module";
 
 @Module({
+    // imports: [PrismaModule], // doing this need to import every time in every module
     controllers: [AuthController],
     providers: [AuthService]
-
 })
 export class AuthModule { }
 
